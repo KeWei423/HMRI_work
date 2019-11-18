d2537de74f937d9ac29c2eab3e2f8ee9cbf12d10
KeWei423 <kewei423@gmail.com>
2019-11-18 14:58:13 -0800

    starts a new project call 'Project_NaCl' ;P

--- BASH_scripts/get-vent-masks.sh
+++ BASH_scripts/get-vent-masks.sh
@@ -15,12 +15,13 @@ do
     cd $dir/mri
     pid=${dir#$fsdir/}
     echo $pid
-    mri_binarize --i aparc+aseg.mgz --match 4 --o $output/"Laterial_vent/"$pid"_L_lat_vent_mask.nii.gz"
-    mri_binarize --i aparc+aseg.mgz --match 5 --o $output/"Inf_Laterial_vent/"$pid"_L_Inf_Lat_vent_mask.nii.gz"
-    mri_binarize --i aparc+aseg.mgz --match 14 --o $output/"third_vent/"$pid"_3rd_Vent_mask.nii.gz"
-    mri_binarize --i aparc+aseg.mgz --match 15 --o $output/"forth_vent/"$pid"_4th_Vent_mask.nii.gz"
-    mri_binarize --i aparc+aseg.mgz --match 43 --o $output/"Laterial_vent/"$pid"_R_lat_vent_mask.nii.gz"
-    mri_binarize --i aparc+aseg.mgz --match 44 --o $output/"Inf_Laterial_vent/"$pid"_R_Inf_Lat_vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 4 --o $output/"Laterial_vent/"$pid"_L_lat_vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 5 --o $output/"Inf_Laterial_vent/"$pid"_L_Inf_Lat_vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 14 --o $output/"third_vent/"$pid"_3rd_Vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 15 --o $output/"forth_vent/"$pid"_4th_Vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 43 --o $output/"Laterial_vent/"$pid"_R_lat_vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 44 --o $output/"Inf_Laterial_vent/"$pid"_R_Inf_Lat_vent_mask.nii.gz"
     #
-    mri_binarize --i aparc+aseg.mgz --match 4 --match 5 --match 14 --match 15 --match 43 --match 44 --o $output/"vent_mask/"$pid"_vent_mask.nii.gz"
+    # mri_binarize --i aparc+aseg.mgz --match 4 --match 5 --match 14 --match 15 --match 43 --match 44 --o $output/"vent_mask/"$pid"_vent_mask.nii.gz"
+    mri_binarize --i aparc+aseg.mgz --match 4 --match 5 --match 14 --match 15 --match 43 --match 44 --o $output/$pid"_vent_mask.nii.gz"
 done
