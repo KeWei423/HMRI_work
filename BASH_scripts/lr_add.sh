aaf42e03c54bb6bea4546b52f7c65fe6fa9f3903
jacek257 <jacek257@gmail.com>
2020-04-02 11:30:27 -0700

    added addition checks to make sure vents exists

--- BASH_scripts/lr_add.sh
+++ BASH_scripts/lr_add.sh
@@ -3,7 +3,7 @@
 root='/home/ke/Desktop/HABLE_vents'
 
 for f in `ls $root`; do
-    if [[ -d $root/$f ]]; then
+    if [[ -d $root/$f && -f $root/$f/${f}_leftlatventricle.nii.gz && -f $root/$f/${f}_rightlatventricle.nii.gz ]]; then
         echo $root/$f/${f}_latventricles.nii.gz
         fslmaths $root/$f/${f}_leftlatventricle.nii.gz -div 4 $root/$f/left_temp.nii.gz
         fslmaths $root/$f/${f}_rightlatventricle.nii.gz -div 43 $root/$f/right_temp.nii.gz
